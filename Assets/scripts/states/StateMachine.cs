a04edc9f5b6a8aad7cdb98dd93bc84ccfcaad478
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-05 08:06:54 -0400

    lowercase foldernames

---
+++ Assets/scripts/states/StateMachine.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+public class StateMachine : MonoBehaviour {
+
+  BaseState currentState;
+
+  void Start() {
+    currentState = GetInitialState();
+    if (currentState != null) {
+      currentState.Enter();
+    }
+  }
+
+  void Update() {
+    if (currentState != null) {
+      currentState.Update();
+    }
+  }
+
+  void LateUpdate() {
+    if (currentState != null) {
+      currentState.LateUpdate();
+    }
+  }
+
+  public void ChangeState(BaseState newState) {
+    if (currentState.name != newState.name) {
+      currentState.Exit();
+      currentState = newState;
+      currentState.Enter();
+    }
+  }
+
+  protected virtual BaseState GetInitialState() {
+    return null;
+  }
+
+  private void OnGUI() {
+    string content = currentState != null ? currentState.name : "(no current state)";
+    GUILayout.Label($"<color='black'><size=60>{content}</size></color>");
+  }
+}
