cfe67176062ea9fcd92193d0acac360cb598a6c9
banzaiMimic <2274050+dr-mimic@users.noreply.github.com>
2021-11-05 23:12:46 -0400

    deleting scripts

--- Assets/scripts/enemies/states/data/SO_ChargeState.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "newChargeStateData", menuName = "Data/StateData/ChargeState")]
-public class SO_ChargeState : ScriptableObject {
-  public float chargeSpeed = 6f;
-  public float chargeTime = 2f;
-}
