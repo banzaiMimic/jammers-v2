1d18cf65e3c959c04fe493d1f4bdc9633e6d3fc0
SuperSoul1029 <66728458+SuperSoul1029@users.noreply.github.com>
2021-11-14 13:43:09 +0530

    Fixing Conflicts

--- Assets/scripts/Player Scripts 2/PlayerMov.cs
+++ Assets/scripts/Player Scripts 2/PlayerMov.cs
@@ -1,30 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMov : MonoBehaviour
 {
     Rigidbody2D rb;
     BoxCollider2D boxCollider;
     Animator anim;
-
+    
     public GameObject landParticleEffect;
     bool canInstantiateLandParticleEffect = true;
     public GameObject feet;
     public LayerMask whatIsGround;
 
+    [Header("Player Movement")]
     public float movSpeed;
     public float jumpForce;
     public float acceleration;
     public float deacceleration;
+
+    [Header("Dash")]
+    public float dashTime;
+    public float dashSpeed;
+    public float dashDeccelarationTime;
+    public float dashCooldownTime;
+    float currentDashSpeed;
+    float dashTimer;
+    float dashLerpTimer;
+    bool isDashing;
+    bool canDash;
+
     float hor;
+
     bool isGrounded;
     bool isFacingRight = true;
 
+    [Header("Recoil")]
+    public GameObject slash;
+    public Vector2 recoilForce;
+    bool recoil;
+
+    //Timers to check if isGrounded/HasPressedJump/(rb.velocity.y < 0) was true in last 0.2 seconds
     float isGroundedRememberTimer;     
     float hasPressedJumpRememberTimer;
     float velocity_Y_RememberTimer;
 
+    [Header("Modify Gravity for jumps")]
     public float holdJumpGravity;   //Gravity when player is holding the jump button
     public float jumpGravity;      //Gravity when player has only pressed the jump button
     public float freeFallGravity;   // Gravity when player is free falling
@@ -36,10 +58,17 @@ public class PlayerMov : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
         anim = GetComponent<Animator>();
+
+        dashTimer = dashTime;
+        currentDashSpeed = dashSpeed;
+        canDash = true;
+        isDashing = false;
     }
 
     void Update()
     {
+        Recoil();
+
         hor = Input.GetAxisRaw("Horizontal");
 
         RotatePlayer();
@@ -47,6 +76,53 @@ public class PlayerMov : MonoBehaviour
         ModifyGravity();
         Animations();
         SpawnLandParticleEffects();
+        Dash();
+    }
+
+    private void Recoil()
+    {
+        recoil = slash.GetComponent<GiveDamage>().doRecoil;
+        if (recoil)
+        {
+            rb.velocity = Vector2.zero;
+
+            float recoilDirection = isFacingRight ? -1 : 1;
+            rb.AddForce(new Vector2(recoilForce.x * recoilDirection, recoilForce.y), ForceMode2D.Impulse);
+        }
+    }
+
+    private void Dash()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift) && canDash && !recoil)
+        {
+            isDashing = true;
+        }
+
+        if (isDashing)
+        {
+            rb.velocity = isFacingRight ? new Vector2(currentDashSpeed, 0f) : new Vector2(-currentDashSpeed, 0f);
+
+            dashTimer -= Time.deltaTime;
+            if (dashTimer < 0)
+            {
+                dashLerpTimer += Time.deltaTime;
+                dashLerpTimer = Mathf.Clamp(dashLerpTimer, 0, 1);
+
+                currentDashSpeed = Mathf.Lerp(currentDashSpeed, movSpeed - 2, dashLerpTimer);
+
+                if (currentDashSpeed <= movSpeed)
+                {
+                    StartCoroutine(enableCanDash());
+
+                    dashTimer = dashTime;
+                    dashLerpTimer = 0f;
+                    isDashing = false;
+                    currentDashSpeed = dashSpeed;
+                    canDash = false;
+                    rb.velocity = Vector2.zero;
+                }
+            }
+        }
     }
 
     private void SpawnLandParticleEffects()
@@ -60,7 +136,7 @@ public class PlayerMov : MonoBehaviour
             velocity_Y_RememberTimer -= Time.deltaTime;
         }
 
-        if (velocity_Y_RememberTimer > 0 && isGrounded && canInstantiateLandParticleEffect)
+        if (velocity_Y_RememberTimer > 0 && isGrounded && canInstantiateLandParticleEffect && !isDashing)
         {
             GameObject effect = Instantiate(landParticleEffect, feet.transform.position, Quaternion.identity);
             Destroy(effect, 1f);
@@ -72,9 +148,12 @@ public class PlayerMov : MonoBehaviour
 
     private void Animations()
     {
-        anim.SetFloat("Velocity_X", Mathf.Abs(rb.velocity.x));
-        anim.SetFloat("Velocity_Y", Mathf.Abs(rb.velocity.y));
-        anim.SetBool("isGrounded", isGrounded);
+        if (!isDashing)
+        {
+            anim.SetFloat("Velocity_X", Mathf.Abs(rb.velocity.x));
+            anim.SetFloat("Velocity_Y", Mathf.Abs(rb.velocity.y));
+            anim.SetBool("isGrounded", isGrounded);
+        }
     }
 
     private void ModifyGravity()
@@ -96,15 +175,18 @@ public class PlayerMov : MonoBehaviour
 
     private void RotatePlayer()
     {
-        if (hor > 0 && !isFacingRight)
+        if (!isDashing && !recoil)
         {
-            transform.rotation = Quaternion.Euler(0, 180, 0);
-            isFacingRight = true;
-        }
-        else if (hor < 0 && isFacingRight)
-        {
-            transform.rotation = Quaternion.Euler(0, 0, 0);
-            isFacingRight = false;
+            if (hor > 0 && !isFacingRight)
+            {
+                transform.rotation = Quaternion.Euler(0, 180, 0);
+                isFacingRight = true;
+            }
+            else if (hor < 0 && isFacingRight)
+            {
+                transform.rotation = Quaternion.Euler(0, 0, 0);
+                isFacingRight = false;
+            }
         }
     }
 
@@ -132,7 +214,7 @@ public class PlayerMov : MonoBehaviour
             hasPressedJumpRememberTimer -= Time.deltaTime;
         }
 
-        if (isGroundedRememberTimer > 0 && hasPressedJumpRememberTimer > 0 && jumpOneTime)
+        if (isGroundedRememberTimer > 0 && hasPressedJumpRememberTimer > 0 && jumpOneTime && !isDashing && !recoil)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             StartCoroutine(EnableJumpOneTime());
@@ -142,26 +224,35 @@ public class PlayerMov : MonoBehaviour
 
     void FixedUpdate()
     {
-        float targetSpeed = hor * movSpeed;  //Top Speed
-        float speedDif = targetSpeed - rb.velocity.x;
+        PlayerMovement();
+    }
 
-        if(speedDif > -0.1f && speedDif < 0.1f)
+    private void PlayerMovement()
+    {
+        if (!isDashing && !recoil)
         {
-            speedDif = 0f;
+            float targetSpeed = hor * movSpeed;  //Top Speed
+            float speedDif = targetSpeed - rb.velocity.x;
+
+            if (speedDif > -0.1f && speedDif < 0.1f)
+            {
+                speedDif = 0f;
+            }
+
+            float accRate;   //Set the value of acceleration
+            if (Mathf.Abs(targetSpeed) > 0.01f)
+            {
+                accRate = acceleration;
+            }
+            else
+            {
+                accRate = deacceleration;
+            }
+
+            float movementForce = speedDif * accRate;
+
+            rb.AddForce(movementForce * Vector2.right);
         }
-
-        float accRate;   //Set the value of acceleration
-        if(Mathf.Abs(targetSpeed) > 0.01f)
-        {
-            accRate = acceleration;
-        } else
-        {
-            accRate = deacceleration;
-        }
-
-        float movementForce = speedDif * accRate;
-
-        rb.AddForce(movementForce * Vector2.right);
     }
 
     IEnumerator EnableLandParticleEffectInstantiation()
@@ -174,5 +265,11 @@ public class PlayerMov : MonoBehaviour
     {
         yield return new WaitForSeconds(0.5f);
         jumpOneTime = true;
+    }
+
+    IEnumerator enableCanDash()
+    {
+        yield return new WaitForSeconds(dashCooldownTime);
+        canDash = true;
     }
 }
